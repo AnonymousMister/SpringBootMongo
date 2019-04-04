2b92c75169520dae9e8e6af926b32dc1fa6c9bf8
zhangxiaofei <13452760334@qq.com>
2019-04-04 14:47:09 +0800

    提交第一版

---
+++ web/src/main/java/com.spring.run/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.spring.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com"})
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ web/src/test/java/com/test/Test.java
@@ -0,0 +1,44 @@
+/*
+ * All rights Reserved, Designed By DataDriver
+ * Copyright: DataDriver.Inc
+ * Company: Zhuo Wo Infomation Technology (ShangHai) CO.LTD
+ */
+package com.test;
+
+import com.mongo.dao.DemoDao;
+import com.mongo.mod.DemoEntity;
+import com.spring.run.DemoApplication;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(classes = DemoApplication.class)
+@RunWith(SpringRunner.class)
+public class Test {
+
+    @Autowired
+    private DemoDao demoDao;
+
+    @org.junit.Test
+    public void  testMongo(){
+        DemoEntity demoEntity = new DemoEntity();
+        demoEntity.setId(1L);
+        demoEntity.setTitle("Spring Boot 中使用 MongoDB");
+        demoEntity.setDescription("关注公众号，搜云库，专注于开发技术的研究与知识分享");
+        demoEntity.setBy("souyunku");
+        demoEntity.setUrl("http://www.souyunku.com");
+
+        demoDao.saveDemo(demoEntity);
+
+        demoEntity = new DemoEntity();
+        demoEntity.setId(2L);
+        demoEntity.setTitle("Spring Boot 中使用 MongoDB");
+        demoEntity.setDescription("关注公众号，搜云库，专注于开发技术的研究与知识分享");
+        demoEntity.setBy("souyunku");
+        demoEntity.setUrl("http://www.souyunku.com");
+
+        demoDao.saveDemo(demoEntity);
+    }
+
+}
